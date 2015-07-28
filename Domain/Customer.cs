bf411f2e6c7ce3dd3126ca9fd69029d87f5c3d09
NattaAllo <natali.pinsker@gmail.com>
2015-07-28 12:46:46 -0700

    Customer added to Domain

---
+++ Domain/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Email { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Coockie { get; set; }
+        public DateTime Birth { get; set; }
+        public DateTime Date { get; set; }
+}
+}
